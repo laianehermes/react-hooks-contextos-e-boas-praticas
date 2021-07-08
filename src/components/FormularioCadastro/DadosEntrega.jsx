fdb4e3082abb648229fa57ac6eae1a625df1658c
lsouza <lsouza@luxfacta.com>
2021-07-08 15:14:44 -0300

    stepper in forms and use hook useEffect

--- src/components/FormularioCadastro/DadosEntrega.jsx
+++ src/components/FormularioCadastro/DadosEntrega.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button } from "@material-ui/core";
 
-function DadosEntrega() {
+function DadosEntrega({ aoEnviar }) {
+  const [cep, setCep] = useState("");
+  const [endereco, setEndereco] = useState("");
+  const [numero, setNumero] = useState("");
+  const [estado, setEstado] = useState("");
+  const [cidade, setCidade] = useState("");
+
   return (
-    <form>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        aoEnviar({ cep, endereco, numero, estado, cidade });
+      }}
+    >
       <TextField
+        value={cep}
+        onChange={(event) => setCep(event.target.value)}
         id='cep'
         label='CEP'
         type='number'
@@ -12,6 +25,8 @@ function DadosEntrega() {
         margin='normal'
       />
       <TextField
+        value={endereco}
+        onChange={(event) => setEndereco(event.target.value)}
         id='endereco'
         label='Endereço'
         type='text'
@@ -20,6 +35,8 @@ function DadosEntrega() {
         fullWidth
       />
       <TextField
+        value={numero}
+        onChange={(event) => setNumero(event.target.value)}
         id='numero'
         label='Número'
         type='number'
@@ -27,6 +44,8 @@ function DadosEntrega() {
         margin='normal'
       />
       <TextField
+        value={estado}
+        onChange={(event) => setEstado(event.target.value)}
         id='estado'
         label='Estado'
         type='text'
@@ -34,6 +53,8 @@ function DadosEntrega() {
         margin='normal'
       />
       <TextField
+        value={cidade}
+        onChange={(event) => setCidade(event.target.value)}
         id='cidade'
         label='Cidade'
         type='text'
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,23 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuarios";
 import DadosEntrega from "./DadosEntrega";
-import { Typography } from "@material-ui/core";
+import { Typography, Stepper, Step, StepLabel } from "@material-ui/core";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
+  const [dadosColetados, setDadosColetados] = useState({});
+  useEffect(() => {
+    if (etapaAtual === formulario.length - 1) {
+      aoEnviar(dadosColetados);
+    }
+  });
 
   const formulario = [
-    <DadosUsuario aoEnviar={proximo} />,
-    <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
-    <DadosEntrega aoEnviar={aoEnviar} />,
+    <DadosUsuario aoEnviar={coletarDados} />,
+    <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
+    <DadosEntrega aoEnviar={coletarDados} />,
+    <Typography variant='h5'>Obrigado pelo Cadastro!</Typography>,
   ];
 
-  function proximo() {
+  function coletarDados(dados) {
+    setDadosColetados({ ...dadosColetados, ...dados });
+    proximo();
+  }
+
+  function proximo(dados) {
     setEtapaAtual(etapaAtual + 1);
   }
 
-  return <>{formulario[etapaAtual]}</>;
+  return <>
+    <Stepper activeStep={etapaAtual}>
+      <Step><StepLabel>Login</StepLabel></Step>
+      <Step><StepLabel>Pessoal</StepLabel></Step>
+      <Step><StepLabel>Entrega</StepLabel></Step>
+      <Step><StepLabel>Finalização</StepLabel></Step>
+    </Stepper>
+  {formulario[etapaAtual]}
+  </>;
 }
 
 export default FormularioCadastro;
