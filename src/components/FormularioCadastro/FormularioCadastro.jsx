181813148ba9e61464fea0e4a98ac125714d1100
lsouza <lsouza@luxfacta.com>
2021-07-08 11:29:10 -0300

    implement step in forms and required fields

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuarios";
 import DadosEntrega from "./DadosEntrega";
+import { Typography } from "@material-ui/core";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
-  return (
-    <>
-      <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
-      <DadosUsuario />
-      <DadosEntrega />
-    </>
-  );
+  const [etapaAtual, setEtapaAtual] = useState(0);
+
+  const formulario = [
+    <DadosUsuario aoEnviar={proximo} />,
+    <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
+    <DadosEntrega aoEnviar={aoEnviar} />,
+  ];
+
+  function proximo() {
+    setEtapaAtual(etapaAtual + 1);
+  }
+
+  return <>{formulario[etapaAtual]}</>;
 }
 
 export default FormularioCadastro;
